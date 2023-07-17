f708f3fc2b7af12c3c77b0f1d305210b383de613
esbnet <esbnet@gmail.com>
2023-07-17 19:52:57 -0300

    feat: :sparkles: create calendar actions

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,7 @@
 import { getWeekDays } from "@/utils/get-week-days";
+import dayjs from "dayjs";
 import { CaretLeft, CaretRight } from "phosphor-react";
+import { useState } from "react";
 import {
   CalendarActions,
   CalendarBody,
@@ -10,19 +12,37 @@ import {
 } from "./styles";
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set("date", 1);
+  });
+
+  function handlePreviousMonth() {
+    const previousMonth = currentDate.subtract(1, "month");
+    setCurrentDate(previousMonth);
+  }
+
+  function handleNextMonth() {
+    const nextMonth = currentDate.add(1, "month");
+    setCurrentDate(nextMonth);
+  }
+
   const shortWeekDays = getWeekDays({ short: true });
+
+  const currentMonth = currentDate.format("MMMM");
+  const currentYear = currentDate.format("YYYY");
+
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Mês anterior">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Mês seguinte">
             <CaretRight />
           </button>
         </CalendarActions>
@@ -38,40 +58,96 @@ export function Calendar() {
         </thead>
         <tbody>
           <tr>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
           </tr>
           <tr>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
           </tr>
           <tr>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
           </tr>
           <tr>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
-            <td><CalendarDay disabled>1</CalendarDay></td>
-            <td><CalendarDay>1</CalendarDay></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
           </tr>
         </tbody>
       </CalendarBody>
--- src/pages/schedule/[username]/SchedulerForm/index.tsx
+++ src/pages/schedule/[username]/SchedulerForm/index.tsx
@@ -1,6 +1,11 @@
+import { CalendarStep } from "./CalendarStep";
 import { ConfirmStep } from "./ConfirmStep";
 
 export default function SchedulerForm() {
-  // return <CalendarStep />;
-  return <ConfirmStep />;
+  return (
+    <>
+      <CalendarStep />
+      <ConfirmStep />
+    </>
+  );
 }
