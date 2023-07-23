249f7ad23e2c77722626b792665ba13ff2a39783
esbnet <esbnet@gmail.com>
2023-07-22 23:23:40 -0300

    feat: :sparkles: create loading page

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -6,13 +6,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+
   if (req.method !== 'POST'){
     return res.status(405).end()
   }
 
   const { name, username } = req.body
 
-  const userAlreadyExists = await prisma.user.findFirst({
+  const userAlreadyExists = await prisma.user.findUnique({
     where: {
       username
     }
@@ -30,10 +31,12 @@ export default async function handler(
       username
     }
   })
+
   setCookie({res}, '@ignitecall:userId', user.id, {
     maxAge: 60 * 60 * 24 * 7, // 7 days
     path: '/'
     
   })
+  
   return res.status(201).json(user)
 }
